64a102ea7cc057c3feb2850c53ccc0e36b4a237e
Zaki Lazuardi Ferysa Putra <zakilfp987@gmail.com>
2024-03-27 13:05:49 +0700

    update sidebar admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -96,7 +96,7 @@
                                 <!-- Menu Item Dashboard -->
 
                                 <!-- Menu Item Calendar -->
-                                <li>
+                                {{-- <li>
                                     <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
                                         href="calendar.html"
                                         @click="selected = (selected === 'Calendar' ? '':'Calendar')"
@@ -110,7 +110,7 @@
 
                                         Calendar
                                     </a>
-                                </li>
+                                </li> --}}
                                 <!-- Menu Item Calendar -->
 
                                 <!-- Menu Item Profile -->
@@ -119,7 +119,20 @@
                                         href="profile.html" @click="selected = (selected === 'Profile' ? '':'Profile')"
                                         :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Profile') && (page === 'profile') }"
                                         :class="page === 'profile' && 'bg-graydark'">
-                                        <svg class="fill-current" width="18" height="18" viewBox="0 0 18 18" fill="none"
+                                        <i class="fa-regular fa-address-card"></i>
+
+                                        Penduduk
+                                    </a>
+                                </li>
+                                <!-- Menu Item Profile -->
+                                
+                                {{-- Menu Data RT --}}
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                                        href="profile.html" @click="selected = (selected === 'Profile' ? '':'Profile')"
+                                        :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Profile') && (page === 'profile') }"
+                                        :class="page === 'profile' && 'bg-graydark'">
+                                        {{-- <svg class="fill-current" width="18" height="18" viewBox="0 0 18 18" fill="none"
                                             xmlns="http://www.w3.org/2000/svg">
                                             <path
                                                 d="M9.0002 7.79065C11.0814 7.79065 12.7689 6.1594 12.7689 4.1344C12.7689 2.1094 11.0814 0.478149 9.0002 0.478149C6.91895 0.478149 5.23145 2.1094 5.23145 4.1344C5.23145 6.1594 6.91895 7.79065 9.0002 7.79065ZM9.0002 1.7719C10.3783 1.7719 11.5033 2.84065 11.5033 4.16252C11.5033 5.4844 10.3783 6.55315 9.0002 6.55315C7.62207 6.55315 6.49707 5.4844 6.49707 4.16252C6.49707 2.84065 7.62207 1.7719 9.0002 1.7719Z"
@@ -127,12 +140,135 @@
                                             <path
                                                 d="M10.8283 9.05627H7.17207C4.16269 9.05627 1.71582 11.5313 1.71582 14.5406V16.875C1.71582 17.2125 1.99707 17.5219 2.3627 17.5219C2.72832 17.5219 3.00957 17.2407 3.00957 16.875V14.5406C3.00957 12.2344 4.89394 10.3219 7.22832 10.3219H10.8564C13.1627 10.3219 15.0752 12.2063 15.0752 14.5406V16.875C15.0752 17.2125 15.3564 17.5219 15.7221 17.5219C16.0877 17.5219 16.3689 17.2407 16.3689 16.875V14.5406C16.2846 11.5313 13.8377 9.05627 10.8283 9.05627Z"
                                                 fill="" />
-                                        </svg>
+                                        </svg> --}}
+                                        <i class="fa-solid fa-people-roof"></i>
 
-                                        Profile
+                                        Data RT
                                     </a>
                                 </li>
-                                <!-- Menu Item Profile -->
+
+                                {{-- Menu Inventaris --}}
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                                        href="profile.html" @click="selected = (selected === 'Profile' ? '':'Profile')"
+                                        :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Profile') && (page === 'profile') }"
+                                        :class="page === 'profile' && 'bg-graydark'">
+                                        {{-- <svg class="fill-current" width="18" height="18" viewBox="0 0 18 18" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M9.0002 7.79065C11.0814 7.79065 12.7689 6.1594 12.7689 4.1344C12.7689 2.1094 11.0814 0.478149 9.0002 0.478149C6.91895 0.478149 5.23145 2.1094 5.23145 4.1344C5.23145 6.1594 6.91895 7.79065 9.0002 7.79065ZM9.0002 1.7719C10.3783 1.7719 11.5033 2.84065 11.5033 4.16252C11.5033 5.4844 10.3783 6.55315 9.0002 6.55315C7.62207 6.55315 6.49707 5.4844 6.49707 4.16252C6.49707 2.84065 7.62207 1.7719 9.0002 1.7719Z"
+                                                fill="" />
+                                            <path
+                                                d="M10.8283 9.05627H7.17207C4.16269 9.05627 1.71582 11.5313 1.71582 14.5406V16.875C1.71582 17.2125 1.99707 17.5219 2.3627 17.5219C2.72832 17.5219 3.00957 17.2407 3.00957 16.875V14.5406C3.00957 12.2344 4.89394 10.3219 7.22832 10.3219H10.8564C13.1627 10.3219 15.0752 12.2063 15.0752 14.5406V16.875C15.0752 17.2125 15.3564 17.5219 15.7221 17.5219C16.0877 17.5219 16.3689 17.2407 16.3689 16.875V14.5406C16.2846 11.5313 13.8377 9.05627 10.8283 9.05627Z"
+                                                fill="" />
+                                        </svg> --}}
+                                        <i class="fa-solid fa-toolbox"></i>
+
+                                        Inventaris
+                                    </a>
+                                </li>
+
+                                {{-- Menu News --}}
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                                        href="profile.html" @click="selected = (selected === 'Profile' ? '':'Profile')"
+                                        :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Profile') && (page === 'profile') }"
+                                        :class="page === 'profile' && 'bg-graydark'">
+                                        {{-- <svg class="fill-current" width="18" height="18" viewBox="0 0 18 18" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M9.0002 7.79065C11.0814 7.79065 12.7689 6.1594 12.7689 4.1344C12.7689 2.1094 11.0814 0.478149 9.0002 0.478149C6.91895 0.478149 5.23145 2.1094 5.23145 4.1344C5.23145 6.1594 6.91895 7.79065 9.0002 7.79065ZM9.0002 1.7719C10.3783 1.7719 11.5033 2.84065 11.5033 4.16252C11.5033 5.4844 10.3783 6.55315 9.0002 6.55315C7.62207 6.55315 6.49707 5.4844 6.49707 4.16252C6.49707 2.84065 7.62207 1.7719 9.0002 1.7719Z"
+                                                fill="" />
+                                            <path
+                                                d="M10.8283 9.05627H7.17207C4.16269 9.05627 1.71582 11.5313 1.71582 14.5406V16.875C1.71582 17.2125 1.99707 17.5219 2.3627 17.5219C2.72832 17.5219 3.00957 17.2407 3.00957 16.875V14.5406C3.00957 12.2344 4.89394 10.3219 7.22832 10.3219H10.8564C13.1627 10.3219 15.0752 12.2063 15.0752 14.5406V16.875C15.0752 17.2125 15.3564 17.5219 15.7221 17.5219C16.0877 17.5219 16.3689 17.2407 16.3689 16.875V14.5406C16.2846 11.5313 13.8377 9.05627 10.8283 9.05627Z"
+                                                fill="" />
+                                        </svg> --}}
+                                        <i class="fa-regular fa-newspaper"></i>
+
+                                        News
+                                    </a>
+                                </li>
+
+                                {{-- Menu Agenda --}}
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                                        href="profile.html" @click="selected = (selected === 'Profile' ? '':'Profile')"
+                                        :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Profile') && (page === 'profile') }"
+                                        :class="page === 'profile' && 'bg-graydark'">
+                                        {{-- <svg class="fill-current" width="18" height="18" viewBox="0 0 18 18" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M9.0002 7.79065C11.0814 7.79065 12.7689 6.1594 12.7689 4.1344C12.7689 2.1094 11.0814 0.478149 9.0002 0.478149C6.91895 0.478149 5.23145 2.1094 5.23145 4.1344C5.23145 6.1594 6.91895 7.79065 9.0002 7.79065ZM9.0002 1.7719C10.3783 1.7719 11.5033 2.84065 11.5033 4.16252C11.5033 5.4844 10.3783 6.55315 9.0002 6.55315C7.62207 6.55315 6.49707 5.4844 6.49707 4.16252C6.49707 2.84065 7.62207 1.7719 9.0002 1.7719Z"
+                                                fill="" />
+                                            <path
+                                                d="M10.8283 9.05627H7.17207C4.16269 9.05627 1.71582 11.5313 1.71582 14.5406V16.875C1.71582 17.2125 1.99707 17.5219 2.3627 17.5219C2.72832 17.5219 3.00957 17.2407 3.00957 16.875V14.5406C3.00957 12.2344 4.89394 10.3219 7.22832 10.3219H10.8564C13.1627 10.3219 15.0752 12.2063 15.0752 14.5406V16.875C15.0752 17.2125 15.3564 17.5219 15.7221 17.5219C16.0877 17.5219 16.3689 17.2407 16.3689 16.875V14.5406C16.2846 11.5313 13.8377 9.05627 10.8283 9.05627Z"
+                                                fill="" />
+                                        </svg> --}}
+                                        <i class="fa-solid fa-calendar-days"></i>
+
+                                        Agenda
+                                    </a>
+                                </li>
+
+                                {{-- Menu Layanan --}}
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                                        href="profile.html" @click="selected = (selected === 'Profile' ? '':'Profile')"
+                                        :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Profile') && (page === 'profile') }"
+                                        :class="page === 'profile' && 'bg-graydark'">
+                                        <i class="fa-solid fa-headset"></i>
+
+                                        Layanan
+                                    </a>
+                                </li>
+
+                                {{-- Menu Data UMKM --}}
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                                        href="profile.html" @click="selected = (selected === 'Profile' ? '':'Profile')"
+                                        :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Profile') && (page === 'profile') }"
+                                        :class="page === 'profile' && 'bg-graydark'">
+                                        <i class="fa-solid fa-store"></i>
+
+                                        Data UMKM
+                                    </a>
+                                </li>
+
+                                {{-- Menu Keuangan --}}
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                                        href="profile.html" @click="selected = (selected === 'Profile' ? '':'Profile')"
+                                        :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Profile') && (page === 'profile') }"
+                                        :class="page === 'profile' && 'bg-graydark'">
+                                        <i class="fa-solid fa-money-bill"></i>
+
+                                        Keuangan
+                                    </a>
+                                </li>
+
+                                {{-- Menu Aspirasi --}}
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                                        href="aspirasi" @click="selected = (selected === 'Profile' ? '':'Profile')"
+                                        :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Profile') && (page === 'profile') }"
+                                        :class="page === 'profile' && 'bg-graydark'">
+                                        <i class="fa-regular fa-lightbulb" width="18"></i>
+
+                                        Aspirasi
+                                    </a>
+                                </li>
+
+                                {{-- Menu Report Data --}}
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                                        href="report_data" @click="selected = (selected === 'Profile' ? '':'Profile')"
+                                        :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Profile') && (page === 'profile') }"
+                                        :class="page === 'profile' && 'bg-graydark'">
+                                        <i class="fa-solid fa-chart-line"></i>
+
+                                        Report Data
+                                    </a>
+                                </li>
 
                                 <!-- Menu Item Forms -->
                                 <li>
