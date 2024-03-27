56a9db98c723095272cb354e00cf94156d039771
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-03-27 16:52:58 +0700

    Remove unnecessary code from admin layout

--- resources/views/components/layout/admin-layout.blade.php
+++ resources/views/components/layout/admin-layout.blade.php
@@ -13,12 +13,27 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 
-<body>
-    {{--
-    <x-partials.admin.nav /> --}}
-    {{ $slot }}
-    {{--
-    <x-partials.admin.footer /> --}}
+<body
+    x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
+    x-init="
+         darkMode = JSON.parse(localStorage.getItem('darkMode'));
+         $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+    :class="{'dark text-bodydark bg-boxdark-2': darkMode === true}">
+    <!-- ===== Preloader Start ===== -->
+    <div x-show="loaded"
+        x-init="window.addEventListener('DOMContentLoaded', () => {setTimeout(() => loaded = false, 500)})"
+        class="fixed left-0 top-0 z-999999 flex h-screen w-screen items-center justify-center bg-white dark:bg-black">
+        <div class="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent">
+        </div>
+    </div>
+    <!-- ===== Preloader End ===== -->
+    <!-- ===== Page Wrapper Start ===== -->
+    <div class="flex h-screen overflow-hidden">
+        <x-partials.admin.sidenav />
+        {{ $slot }}
+        {{--
+        <x-partials.admin.footer /> --}}
+    </div>
 </body>
 <script src="{{ asset('assets/js/flowbite.min.js') }}"></script>
 <script defer src="{{ asset('assets/js/bundle.js') }}"></script>
