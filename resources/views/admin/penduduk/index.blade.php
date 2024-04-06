e8bd3472294be2ad20a00887e7900b451c75cafc
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-04-06 22:52:12 +0700

    Add 'Detail' link to penduduk index page

--- resources/views/admin/penduduk/index.blade.php
+++ resources/views/admin/penduduk/index.blade.php
@@ -87,6 +87,8 @@
                             <td class="px-6 py-4">
                                 <a href="#"
                                     class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                <a href="#"
+                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Detail</a>
                             </td>
                         </tr>
                     @endforeach
