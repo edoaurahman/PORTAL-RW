458e335dc3f64bdd026c6c5ff63de95ec785fc8e
Putra Nindya Yuwana <137630717+ptryuwana@users.noreply.github.com>
2024-04-06 23:04:36 +0700

    adjust header layout dan header sidenav

--- resources/views/components/partials/admin/header.blade.php
+++ resources/views/components/partials/admin/header.blade.php
@@ -29,9 +29,9 @@
                 </span>
             </button>
             <!-- Hamburger Toggle BTN -->
-            <a class="block flex-shrink-0 lg:hidden" href="index.html">
-                <img src="{{ asset('assets/images/logo/logo.svg') }}" alt="Logo" />
-            </a>
+            {{-- <a class="block flex-shrink-0 lg:hidden" href="index.html">
+                <img :src="darkMode ? '{{ asset('assets/images/logo/logo-light.png') }}' : '{{ asset('assets/images/logo/logo-dark.png') }}'" width="40px" height="auto" class="ml-2 duration-300" alt="Logo" />
+            </a> --}}
         </div>
         <div class="hidden sm:block">
             <form action="https://formbold.com/s/unique_form_id" method="POST">
