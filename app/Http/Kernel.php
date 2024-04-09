00ebb3332e0e0ccca29d3cf8883ddeff8722aa1f
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-04-09 14:13:16 +0700

    Add isRw middleware to Http/Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'isSuperAdmin' => \App\Http\Middleware\IsSuperAdmin::class,
+        'isRw' => \App\Http\Middleware\isRw::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
