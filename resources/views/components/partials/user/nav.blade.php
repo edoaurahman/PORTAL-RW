2cbdf107a4797706ae3a22b760ad5b49247389ca
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-05-02 19:43:56 +0700

    Update user navigation to display user's name and NIK, and add tgl_bayar column to tb_keuangan table, and update PendudukSeeder with new data

--- resources/views/components/partials/user/nav.blade.php
+++ resources/views/components/partials/user/nav.blade.php
@@ -64,17 +64,18 @@
                 </li>
             </ul>
             <div>
-
-                <div id="avatarButton" type="button" data-dropdown-toggle="userDropdown"
-                    data-dropdown-placement="bottom-start" class="rounded-full cursor-pointer" alt="User dropdown">
-                    <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                        <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                                clip-rule="evenodd"></path>
-                        </svg>
+                @auth('web')
+                    <div id="avatarButton" type="button" data-dropdown-toggle="userDropdown"
+                        data-dropdown-placement="bottom-start" class="rounded-full cursor-pointer" alt="User dropdown">
+                        <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                            <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </div>
                     </div>
-                </div>
+                @endauth
                 <!-- Dropdown menu -->
                 <div id="userDropdown"
                     class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
