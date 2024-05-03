a25cc8c65ded3533cfaa075c1ce903d2462fca2b
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-05-03 09:48:07 +0700

    Create migration file for tb_gambar_umkm table

---
+++ database/migrations/2024_05_03_024045_create_tb_gambar_umkm_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_gambar_umkm', function (Blueprint $table) {
+            $table->integer('id_gambar_umkm')->autoIncrement();
+            $table->string('gambar', 100);
+            $table->string('deskripsi', 300);
+            $table->integer('id_umkm');
+            $table->foreign('id_umkm')->references('id_umkm')->on('tb_umkm')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_gambar_umkm');
+    }
+};
