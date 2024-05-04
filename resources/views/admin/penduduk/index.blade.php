aca6f837e91d4333910c90d6b0eae72488461295
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-05-04 14:50:42 +0700

    chore: Remove unused code and comments from admin/penduduk/akun.blade.php and admin/penduduk/index.blade.php

--- resources/views/admin/penduduk/index.blade.php
+++ resources/views/admin/penduduk/index.blade.php
@@ -146,25 +146,5 @@
                 }
             }
         }
-
-        // function show delete modal with swalalert2
-        function showDeleteModal(e) {
-            e.preventDefault();
-            console.log(e);
-            // Swal.fire({
-            //     title: 'Are you sure?',
-            //     text: "You won't be able to revert this!",
-            //     icon: 'warning',
-            //     showCancelButton: true,
-            //     confirmButtonColor: '#3085d6',
-            //     cancelButtonColor: '#d33',
-            //     confirmButtonText: 'Yes, delete it!'
-            // }).then((result) => {
-            //     if (result.isConfirmed) {
-            //         return true;
-            //     }
-            //     return false;
-            // })
-        }
     </script>
 </x-layout.admin-layout>
