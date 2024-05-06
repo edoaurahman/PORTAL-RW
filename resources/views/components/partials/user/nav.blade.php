e90c52ad98390462c0e684c05eda7531a7fe26c7
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-05-06 17:40:37 +0700

    chore: Update navbar styling and fix selected page highlighting

--- resources/views/components/partials/user/nav.blade.php
+++ resources/views/components/partials/user/nav.blade.php
@@ -14,24 +14,24 @@
                     d="M1 1h15M1 7h15M1 13h15" />
             </svg>
         </button>
-        <div class="hidden w-full md:flex md:flex-row md:w-auto" id="navbar-default" x-data="{ selected: location.pathname }" >
+        <div class="hidden w-full md:flex md:flex-row md:w-auto" id="navbar-default" x-data="{ selected: location.pathname }">
             <ul
                 class="font-medium flex flex-col md:items-center p-4 md:p-0 mt-4 border  rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
                 <li>
                     <a href="{{ route('user.home') }}"
                         class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                         aria-current="page"
-                        :class="{ 'text-blue-700 dark:text-gray-400': (selected.includes('home')) }">Home</a>
+                        :class="{ '!text-blue-700 !dark:text-gray-400': (selected.includes('home')) }">Home</a>
                 </li>
                 <li>
                     <a href="{{ route('user.penduduk') }}"
                         class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                        :class="{ 'text-blue-700 dark:text-gray-400': (selected.includes('penduduk')) }">Penduduk</a>
+                        :class="{ '!text-blue-700 !dark:text-gray-400': (selected.includes('penduduk')) }">Penduduk</a>
                 </li>
                 <li>
                     <a href="{{ route('user.beritaa') }}"
                         class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                        :class="{ 'text-blue-700 dark:text-gray-400': (selected.includes('beritaa')) }">Berita</a>
+                        :class="{ '!text-blue-700 !dark:text-gray-400': (selected.includes('beritaa')) }">Berita</a>
                 </li>
                 <li>
                     <a href="#"
