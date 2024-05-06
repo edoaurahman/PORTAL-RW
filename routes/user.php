ba40d5a1453be459c2936ad2e2c28fda4f553c2e
CyberPanel <support@cyberpanel.net>
2024-05-06 17:21:41 +0700

    fix user route

--- routes/user.php
+++ routes/user.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\penduduk\PendudukController;
+use App\Http\Controllers\Penduduk\PendudukController;
 
 Route::prefix('penduduk')->group(function () {
     Route::middleware(['auth'])->group(function () {
