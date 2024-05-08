07753d690e0496d40927c0bcc0934c545d4ec3b0
Putra Nindya Yuwana <137630717+ptryuwana@users.noreply.github.com>
2024-05-08 10:18:03 +0700

    Update level

--- resources/views/admin/level.blade.php
+++ resources/views/admin/level.blade.php
@@ -48,13 +48,10 @@
                 </div>
             @endforeach
         @endif
-        <div class="flex flex-nowrap overflow-x-auto shadow-md sm:rounded-md">
+        <div class="flex flex-nowrap relative overflow-x-auto shadow-md sm:rounded-md">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="p-4">
-    
-                        </th>
                         <th scope="col" class="px-6 py-3">
                             Nama
                         </th>
@@ -73,8 +70,6 @@
                 <tbody>
                     @foreach ($users as $item)
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td class="flex flex-nowrap w-4 p-4">
-                    </td>
                             <th scope="row"
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {{ $item->penduduk->nama }}
