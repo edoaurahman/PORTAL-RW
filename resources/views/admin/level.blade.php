e5e81156b3c8c9640fde18eaac04c7b34bcf620a
Putra Nindya Yuwana <137630717+ptryuwana@users.noreply.github.com>
2024-05-08 11:35:36 +0700

    update form level

--- resources/views/admin/level.blade.php
+++ resources/views/admin/level.blade.php
@@ -144,7 +144,7 @@
                             </span>
                             <input name="nik"
                                 class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                type="text" autocomplete="off" list="myList" id="nik" placeholder="Nik" />
+                                type="text" autocomplete="off" list="myList" id="nik" placeholder="NIK" />
                             <datalist id="myList">
                                 @foreach ($penduduk as $item)
                                     <option value="{{ $item->penduduk->nik }}">{{ $item->penduduk->nama }}</option>
