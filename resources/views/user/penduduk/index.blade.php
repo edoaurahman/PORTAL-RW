3d226da817bd90b51f7167bd76be177d357ec01d
abidgymnastiar <abidgymnastiar@gmail.com>
2024-05-15 08:54:52 +0700

    page_penduduk

--- resources/views/user/penduduk/index.blade.php
+++ resources/views/user/penduduk/index.blade.php
@@ -1,19 +1,21 @@
 <x-layout.user-layout>
     <section class="hero max-w-6xl mx-auto font-sans pb-12 pt-[100px] ">
         <div class="flex flex-row items-center justify-between gap-x-20">
-            <div class="flex-1 flex-col gap-y-10">
+            <div class="flex-1 flex-col gap-y-10 space-y-5">
                 <div class="w-fit gap-x-2 px-4 py-2 small-badge flex flex-row bg-white rounded-full items-center">
                     <i class="fa-solid fa-globe"></i>
                     <p class=" text-base font-semibold text-black2">
-                        Kami memiliki jumlah penduduk lebih dari 1.000 jiwa.
+                        Profil data-data penduduk RT {{ $user->penduduk->alamat->rt }}
                     </p>
                 </div>
-                <div class="gap-y-2 flex flex-col">
+                <div class="gap-y-2 flex flex-col space-y-2">
                     <h1 class="text-black1 font-bold text-[70px] leading-none">Data Seluruh<br>
-                        Penduduk RW 02.
+                        Penduduk RT {{ $user->penduduk->alamat->rt }}.
                     </h1>
                     <div class="text-base leading-loose text-black3">
-                        Data seluruh penduduk RW 02 adalah fondasi penting dalam perencanaan dan pembangunan wilayah.
+                        Data seluruh penduduk RT {{ $user->penduduk->alamat->rt }} adalah fondasi penting dalam
+                        perencanaan dan
+                        pembangunan wilayah.
                         Informasi ini, termasuk jumlah dan profil penduduk, menjadi dasar bagi pemerintah lokal untuk
                         merancang kebijakan yang lebih efektif dan responsif terhadap kebutuhan masyarakat.
                     </div>
@@ -37,7 +39,8 @@
                             </div>
                             <div>
                                 <h5 class="leading-none text-2xl font-bold text-gray-900 dark:text-white pb-1">3.4k</h5>
-                                <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Leads generated per week
+                                <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Total penduduk di RT
+                                    {{ $user->penduduk->alamat->rt }}
                                 </p>
                             </div>
                         </div>
@@ -56,72 +59,17 @@
 
                     <div class="grid grid-cols-2">
                         <dl class="flex items-center">
-                            <dt class="text-gray-500 dark:text-gray-400 text-sm font-normal me-1">Money spent:</dt>
+                            <dt class="text-gray-500 dark:text-gray-400 text-sm font-normal me-1">Laki-laki:</dt>
                             <dd class="text-gray-900 text-sm dark:text-white font-semibold">$3,232</dd>
                         </dl>
                         <dl class="flex items-center justify-end">
-                            <dt class="text-gray-500 dark:text-gray-400 text-sm font-normal me-1">Conversion rate:</dt>
+                            <dt class="text-gray-500 dark:text-gray-400 text-sm font-normal me-1">Perempuan:</dt>
                             <dd class="text-gray-900 text-sm dark:text-white font-semibold">1.2%</dd>
                         </dl>
                     </div>
 
                     <div id="column-chart"></div>
-                    <div
-                        class="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between">
-                        <div class="flex justify-between items-center pt-5">
-                            <!-- Button -->
-                            <button id="dropdownDefaultButton" data-dropdown-toggle="lastDaysdropdown"
-                                data-dropdown-placement="bottom"
-                                class="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white"
-                                type="button">
-                                Last 7 days
-                                <svg class="w-2.5 m-2.5 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 10 6">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="m1 1 4 4 4-4" />
-                                </svg>
-                            </button>
-                            <!-- Dropdown menu -->
-                            <div id="lastDaysdropdown"
-                                class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
-                                    aria-labelledby="dropdownDefaultButton">
-                                    <li>
-                                        <a href="#"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Yesterday</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Today</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
-                                            7 days</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
-                                            30 days</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
-                                            90 days</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <a href="#"
-                                class="uppercase text-sm font-semibold inline-flex items-center rounded-lg text-blue-600 hover:text-blue-700 dark:hover:text-blue-500  hover:bg-gray-100 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 px-3 py-2">
-                                Leads Report
-                                <svg class="w-2.5 h-2.5 ms-1.5 rtl:rotate-180" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="m1 9 4-4-4-4" />
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
+
                 </div>
 
             </div>
