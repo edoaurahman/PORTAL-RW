83c49759babcd9c605476e8dcde663bbc39c6082
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-05-18 19:45:12 +0700

    refactor: Add KategoriKeuanganModel and KeuanganModel for managing financial categories and transactions

---
+++ app/Models/KeuanganModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KeuanganModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tb_keuangan';
+    protected $primaryKey = 'id_keuangan';
+    protected $fillable = [
+        'id_kategori',
+        'nik',
+        'jumlah',
+        'keterangan',
+    ];
+}
