c0d408cd36def2437bcefde2ec3b931508e17392
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-05-20 05:52:04 +0700

    refactor: Update image paths in BeritaSeeder and riwayatBerita.blade.php

--- resources/views/user/berita/riwayatBerita.blade.php
+++ resources/views/user/berita/riwayatBerita.blade.php
@@ -32,7 +32,7 @@
     </section>
     {{-- List Berita --}}
     <div class="historyberita max-w-6xl mx-auto font-sans pb-12">
-        @if ($berita)
+        @if ($berita == null || $berita->isEmpty())
             <div class="flex items-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
                 role="alert">
                 <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -40,9 +40,8 @@
                     <path
                         d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
                 </svg>
-                <span class="sr-only">Info</span>
                 <div>
-                    <span class="font-medium">Info alert!</span> Anda belum memiliki berita.
+                    Anda belum memiliki berita.
                 </div>
             </div>
         @endif
