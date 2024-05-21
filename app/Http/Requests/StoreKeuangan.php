86f72b18eab8571e8b24db7f939fda71307284a0
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-05-21 19:52:10 +0700

    chore: Update form validation rules for StoreKeuangan request

--- app/Http/Requests/StoreKeuangan.php
+++ app/Http/Requests/StoreKeuangan.php
@@ -22,21 +22,18 @@ class StoreKeuangan extends FormRequest
     public function rules(): array
     {
         return [
-            'id_kategori' => 'required|exists:tb_kategori_keuangan,id_kategori',
             'jumlah' => 'required|numeric',
-            'nik' => 'required|exists:tb_penduduk,nik'
+            'no_kk' => 'required|exists:tb_kk,no_kk'
         ];
     }
 
     public function messages()
     {
         return [
-            'id_kategori.required' => 'Kategori keuangan wajib diisi',
-            'id_kategori.exists' => 'Kategori keuangan tidak ditemukan',
             'jumlah.required' => 'Jumlah wajib diisi',
             'jumlah.numeric' => 'Jumlah harus berupa angka',
-            'nik.required' => 'NIK wajib diisi',
-            'nik.exists' => 'NIK tidak ditemukan'
+            'no_kk.required' => 'No KK wajib diisi',
+            'no_kk.exists' => 'No KK tidak ditemukan'
         ];
     }
 }
