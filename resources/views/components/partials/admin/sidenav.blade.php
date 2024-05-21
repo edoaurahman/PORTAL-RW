fdb9ec44750be08904a4c48692aa372a23023443
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-05-21 19:55:03 +0700

    add nnavigation link to sidenav

--- resources/views/components/partials/admin/sidenav.blade.php
+++ resources/views/components/partials/admin/sidenav.blade.php
@@ -25,7 +25,7 @@
         <nav class="mt-5 px-4 py-4 lg:mt-9 lg:px-6">
             <!-- Menu Group -->
             <div>
-                
+
                 <ul class="mb-5 flex flex-col gap-1.5">
                     <!-- Menu Item Dashboard -->
                     <li>
@@ -38,8 +38,9 @@
                     </li>
                     <!-- Menu Item Dashboard -->
 
-                    <h3 class="mb-3 mt-4 ml-4 text-sm font-medium text-black dark:text-white duration-300">Menu Kependudukan</h3>
-                    
+                    <h3 class="mb-3 mt-4 ml-4 text-sm font-medium text-black dark:text-white duration-300">Menu
+                        Kependudukan</h3>
+
                     <!-- Menu Item Level -->
                     <li x-show="levelUser === 'Super Admin' || levelUser === 'RW'">
                         <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-graydark duration-300 ease-in-out hover:bg-ungu hover:text-white dark:hover:bg-meta-4 dark:text-white"
@@ -106,7 +107,8 @@
                         </a>
                     </li>
 
-                    <h3 class="mb-3 mt-4 ml-4 text-sm font-medium text-black dark:text-white duration-300">Menu Kesejahteraan Masyarakat</h3>   
+                    <h3 class="mb-3 mt-4 ml-4 text-sm font-medium text-black dark:text-white duration-300">Menu
+                        Kesejahteraan Masyarakat</h3>
 
                     <!-- Menu Bansos -->
                     <li>
@@ -149,7 +151,7 @@
                             Aspirasi
                         </a>
                     </li>
-                    
+
                     {{-- Menu Inventaris --}}
                     <li>
                         <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-graydark duration-300 ease-in-out hover:bg-ungu hover:text-white dark:hover:bg-meta-4 dark:text-white"
@@ -174,17 +176,50 @@
                         </a>
                     </li>
 
-                    <h3 class="mb-3 mt-4 ml-4 text-sm font-medium text-black dark:text-white duration-300">Menu Laporan RT/RW</h3>
+                    <h3 class="mb-3 mt-4 ml-4 text-sm font-medium text-black dark:text-white duration-300">Menu Laporan
+                        RT/RW</h3>
 
                     {{-- Menu Keuangan --}}
                     <li>
                         <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-graydark duration-300 ease-in-out hover:bg-ungu hover:text-white dark:hover:bg-meta-4 dark:text-white"
-                            href="{{ route('admin.keuangan') }}"
+                            href="#" @click.prevent="selected = (selected.includes('keuangan') ? '':'keuangan')"
                             :class="{ 'bg-ungu text-white dark:bg-meta-4': (selected.includes('keuangan')) }">
-                            <i class="fa-solid fa-money-bill"></i>
-
+                            <i class="fa-solid fa-id-card"></i>
                             Keuangan
+                            <svg class="absolute right-4 top-1/2 -translate-y-1/2 fill-current"
+                                :class="{ 'rotate-180': (selected.includes('keuangan')) }" width="20"
+                                height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                                    fill="" />
+                            </svg>
                         </a>
+
+                        <!-- Dropdown Menu Start -->
+                        <div class="translate transform overflow-hidden"
+                            :class="(selected.includes('keuangan')) ? 'block' : 'hidden'">
+                            <ul class="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-graydark dark:text-white duration-300 ease-in-out hover:text-ungu"
+                                        :class="{ 'text-ungu dark:bg-meta-4': (selected == '/admin/keuangan') }"
+                                        href="{{ route('admin.keuangan') }}">Pembayaran
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-graydark dark:text-white duration-300 ease-in-out hover:text-ungu"
+                                        :class="{ 'text-ungu dark:bg-meta-4': (selected == '/admin/keuangan/riwayat') }"
+                                        href="{{ route('admin.keuangan.riwayat') }}">Riwayat Pembayaran
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-graydark dark:text-white duration-300 ease-in-out hover:text-ungu"
+                                        :class="{ 'text-ungu dark:bg-meta-4': (selected == '/admin/keuangan/setting') }"
+                                        href="{{ route('admin.keuangan.setting') }}">Settings
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- Dropdown Menu End -->
                     </li>
 
                     {{-- Menu Report Data --}}
