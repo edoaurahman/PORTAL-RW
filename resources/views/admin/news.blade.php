5119e9df2c671a116bcb3aa389445fd10ee001ae
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-05-21 19:55:19 +0700

    fix some errors

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -15,7 +15,7 @@
         <div class="flex justify-between pb-5">
             <h1> </h1>
             <!-- Modal toggle -->
-            <button data-modal-target="add-penduduk" data-modal-toggle="add-penduduk"
+            <button
                 class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 type="button">
                 <a href="{{ route('user.berita.dashboard') }}">Masuk Dashboard</a>
