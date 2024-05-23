6f11803b27c5e067db1c9ba5f8ebe91278d0d6b6
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-05-23 17:21:09 +0700

    Add endpoint get data agenda

--- app/Http/Controllers/Api/AgendaController.php
+++ app/Http/Controllers/Api/AgendaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\AgendaModel;
+use App\Models\PendudukModel;
 use Illuminate\Http\Request;
 
 class AgendaController extends Controller
@@ -11,8 +12,10 @@ class AgendaController extends Controller
     public function index(Request $request)
     {
         // agenda pada bulan ini dan tahun ini
-        $date = $request->date ?? date('Y-m');
-        $agenda = AgendaModel::where('start', 'like', $date . '%')->get();
+        $date = $request->date ? date('Y-m', strtotime($request->date)) : date('Y-m');
+        $agenda = AgendaModel::where('start', 'like', $date . '%')
+            ->orderBy('start')
+            ->get();
         return response()->json($agenda);
     }
 }
