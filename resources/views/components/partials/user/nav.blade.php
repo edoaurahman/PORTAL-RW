d6ccaf10c0b2cb4ef264e2df17d65fd0f8bbfb8a
Muhamad Anang <ananghusnul10@gmail.com>
2024-05-24 11:33:01 +0700

    update navbar

--- resources/views/components/partials/user/nav.blade.php
+++ resources/views/components/partials/user/nav.blade.php
@@ -4,7 +4,7 @@
             <img :src="darkMode ? '{{ asset('assets/images/logo/logo-light.png') }}' :
                 '{{ asset('assets/images/logo/logo-dark.png') }}'"
                 width="40px" height="auto" class="duration-300" alt="Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">02 ARJOSARI</span>
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
         </a>
         <button data-collapse-toggle="navbar-default" type="button"
             class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
