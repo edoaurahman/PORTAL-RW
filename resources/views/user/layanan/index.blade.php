70d66aa39cb4fb61cc4ca58ad8d59e4a80ead359
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-05-31 21:41:29 +0700

    chore: Refactor image display in layanan index page

--- resources/views/user/layanan/index.blade.php
+++ resources/views/user/layanan/index.blade.php
@@ -16,7 +16,9 @@
 
                 </div>
                 <div class="flex flex-row gap-x-4 items-center">
-                    <a href="#tutorial" class="hover:bg-indigo-900 text-base bg-ungu text-white py-4 px-10 rounded-full font-semibold">Cara Menggunakan layanan</a>
+                    <a href="#tutorial"
+                        class="hover:bg-indigo-900 text-base bg-ungu text-white py-4 px-10 rounded-full font-semibold">Cara
+                        Menggunakan layanan</a>
 
                 </div>
 
@@ -31,38 +33,45 @@
         <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
 
             @foreach ($layanan as $item)
-            
-            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                    <img class="rounded-t-lg h-70 bg-purple-400 " src="{{ $item->getImage() }}" alt="" />
-                </a>
-                <div class="p-5">
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
-                        <h5 class="mb-2 text-[20px] font-bold tracking-tight text-gray-900 dark:text-white">{{ $item->nama_surat }}</h5>
+                        <img class="rounded-t-lg h-70 bg-purple-400 object-cover" src="{{ $item->getImage() }}"
+                            alt="" />
                     </a>
-                    <p class="mb-3  font-normal text-gray-700 dark:text-gray-400">{{ $item->keterangan }}</p>
-                    <a href="{{ $item->downloadSurat() }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center border text-ungu bg-white border-ungu rounded-lg hover:bg-ungu hover:text-white focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:text-white dark:hover:bg-purple-700 dark:focus:ring-purple-800">
-                        Gunakan Layanan
-                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                        </svg>
-                    </a>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5 class="mb-2 text-[20px] font-bold tracking-tight text-gray-900 dark:text-white">
+                                {{ $item->nama_surat }}</h5>
+                        </a>
+                        <p class="mb-3  font-normal text-gray-700 dark:text-gray-400">{{ $item->keterangan }}</p>
+                        <a href="{{ $item->downloadSurat() }}"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center border text-ungu bg-white border-ungu rounded-lg hover:bg-ungu hover:text-white focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:text-white dark:hover:bg-purple-700 dark:focus:ring-purple-800">
+                            Gunakan Layanan
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
                 </div>
-            </div>
             @endforeach
 
         </div>
     </section>
 
-    <section id="tutorial" class=" max-w-6xl pt-20 mx-auto font-sans pb-12 mb-60 bg-ungu p-14 rounded-3xl z-20 relative ">
+    <section id="tutorial"
+        class=" max-w-6xl pt-20 mx-auto font-sans pb-12 mb-60 bg-ungu p-14 rounded-3xl z-20 relative ">
         <div class=" items-center justify-between">
-            <h5 class="mb-2 text-[20px] font-bold tracking-tight text-white dark:text-white">Cara Menggunakan Layanan Pak RW</h5>
+            <h5 class="mb-2 text-[20px] font-bold tracking-tight text-white dark:text-white">Cara Menggunakan Layanan
+                Pak RW</h5>
             <h1 class="leading-loose text-white dark:text-white">
                 1. Jika Layanan tersebut berupa surat, maka tahap pertama adalah mendowload surat tersebut. <br>
                 2. Surat yang membutuhkan tanda tangan RT/RW, sudah terlegalisir. <br>
                 3. Setelah di download, isi data-data yang diperlukan, baik diedit atau secara manual. <br>
                 4. setelah berbentuk hard file, antar kepada RW untuk diproses lebih lanjut.
-            </h1>   
+            </h1>
         </div>
     </section>
 
