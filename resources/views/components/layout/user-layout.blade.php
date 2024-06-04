ef884aeffad69d4e3dbd043d626f44425c367a88
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-06-04 10:00:15 +0700

    chore: Update font-awesome library to version 6.5.2

--- resources/views/components/layout/user-layout.blade.php
+++ resources/views/components/layout/user-layout.blade.php
@@ -7,10 +7,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PORTAL RW</title>
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.min.css') }}"> --}}
+    {{-- <script src="{{ asset('assets/js/fontawesome.min.js') }}"></script> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js"
+        integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="{{ asset('assets/css/flowbite.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/user/style.css') }}">
-    <script src="{{ asset('assets/js/fontawesome.min.js') }}"></script>
     <script defer src="{{ asset('assets/js/bundle.js') }}"></script>
     <script src="{{ asset('assets/js/flowbite.min.js') }}"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
