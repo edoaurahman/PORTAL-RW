8258ebc9816714933b88b58cb73f65d0363f2660
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-06-06 18:56:52 +0700

    fix print report data

--- app/Http/Controllers/Admin/ReportDataController.php
+++ app/Http/Controllers/Admin/ReportDataController.php
@@ -3,59 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\AlamatModel;
-use App\Models\KeuanganModel;
-use App\Models\KkModel;
-use App\Models\PendudukModel;
-use App\Models\PengeluaranModel;
-use Illuminate\Http\Request;
-use Spatie\Browsershot\Browsershot;
 
 class ReportDataController extends Controller
 {
     public function index()
     {
-        $jumlahRT = AlamatModel::distinct('rt')->count('rt');
-
-        $totalPenduduk = PendudukModel::count();
-
-        // Mengambil jumlah penduduk dengan status pendatang
-        $pendudukPendatang = PendudukModel::where('status_penduduk', 'Pendatang')->count();
-
-        // Mengambil jumlah penduduk dengan status tetap
-        $pendudukTetap = PendudukModel::where('status_penduduk', 'Penduduk Tetap')->count();
-
-        $jumlahKK = KkModel::count();
-
-        return view("admin.report-data", compact('jumlahRT', 'totalPenduduk', 'pendudukPendatang', 'pendudukTetap', 'jumlahKK'));
-    }
-
-    public function downloadPdf(Request $request)
-    {
-        $url = 'http://' . $request->getHttpHost() . '/report-data/preview';
-        // dd($url);
-        Browsershot::url($url)->setNodeModulePath(base_path('node_modules'))->landscape()->savePdf(public_path('/storage/report.pdf'));
-        return response()->download(public_path('/storage/report.pdf'));
-    }
-
-    public function preview()
-    {
-        $jumlahRT = AlamatModel::distinct('rt')->count('rt');
-
-        $totalPenduduk = PendudukModel::count();
-
-        // Mengambil jumlah penduduk dengan status pendatang
-        $pendudukPendatang = PendudukModel::where('status_penduduk', 'Pendatang')->count();
-
-        // Mengambil jumlah penduduk dengan status tetap
-        $pendudukTetap = PendudukModel::where('status_penduduk', 'Penduduk Tetap')->count();
-
-        $jumlahKK = KkModel::count();
-
-        $pemasukkan = KeuanganModel::sum('jumlah');
-        $pengeluaran = PengeluaranModel::sum('jumlah');
-        $totalKeuangan = $pemasukkan - $pengeluaran;
-
-        return view("preview", compact('jumlahRT', 'totalPenduduk', 'pendudukPendatang', 'pendudukTetap', 'jumlahKK', 'totalKeuangan', 'pemasukkan', 'pengeluaran'));
+        return view("admin.report-data");
     }
 }
--- resources/views/admin/report-data.blade.php
+++ resources/views/admin/report-data.blade.php
@@ -10,13 +10,9 @@
                 Report Data
             </small>
         </h3>
-
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-            integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-            crossorigin="anonymous" referrerpolicy="no-referrer" />
         <div class="py-4 px-16 dark:bg-[#ffffff] dark:text-black" id="report-data">
             <div class="fixed end-6 bottom-6 group animate-bounce">
-                <button type="button" {{-- onclick="printJS('printJS-form', 'html')" --}} id="downloadPdfBtn" aria-controls="speed-dial-menu-default"
+                <button type="button" onclick="printJS('printJS-form', 'html')" aria-controls="speed-dial-menu-default"
                     aria-expanded="false"
                     class="flex items-center justify-center text-white bg-blue-700 rounded-full w-14 h-14 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
                     <i class="fa-solid fa-print  text-xl"></i>
@@ -24,14 +20,8 @@
                 </button>
             </div>
             <x-partials.admin.report-data.preview />
-
-
         </div>
+        <link rel="stylesheet" href="https://printjs-4de6.kxcdn.com/print.min.css">
+        <script src="https://printjs-4de6.kxcdn.com/print.min.js"></script>
     </div>
 </x-layout.admin-layout>
-<script>
-    document.getElementById('downloadPdfBtn').addEventListener('click', function() {
-        // go to /report-data/preview on new tab
-        window.open('/report-data/download', '_blank');
-    });
-</script>
