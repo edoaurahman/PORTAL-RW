8a81fa884ef225f17be639967a5b78db4624f71f
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-06-08 00:37:35 +0700

    refactor: Add transaction for pinjam method in InventarisController

--- app/Http/Controllers/User/InventarisController.php
+++ app/Http/Controllers/User/InventarisController.php
@@ -7,23 +7,27 @@ use App\Http\Controllers\Controller;
 use App\Models\InventarisModel;
 use App\Models\PeminjamanModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class InventarisController extends Controller
 {
     public function index()
-    {        
+    {
         $inventaris = InventarisModel::all();
         return view('user.inventaris.index', compact('inventaris'));
     }
 
     public function pinjam(Request $request)
     {
-        $inventaris = new PeminjamanModel();
-        $inventaris->id_inventaris = $request->id_inventaris;
-        $inventaris->jumlah = $request->jumlah;
-        $inventaris->nik = auth()->user()->nik;
-        $inventaris->status = 'pending';
-        $inventaris->save();
+        DB::transaction(function () use ($request) {
+            $inventaris = new PeminjamanModel();
+            $inventaris->id_inventaris = $request->id_inventaris;
+            $inventaris->jumlah = $request->jumlah;
+            $inventaris->nik = auth()->user()->nik;
+            $inventaris->status = 'pending';
+            $inventaris->save();
+        });
+
         return redirect()->route('user.inventaris')->with('success', 'Barang berhasil dipinjam');
     }
 }
