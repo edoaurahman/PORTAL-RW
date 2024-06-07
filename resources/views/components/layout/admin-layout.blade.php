a2e44102d2e6a08697ae83c105ce800ea32cee18
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-06-07 22:35:04 +0700

    refactor: Update admin layout preloader spinner color

--- resources/views/components/layout/admin-layout.blade.php
+++ resources/views/components/layout/admin-layout.blade.php
@@ -23,7 +23,7 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
     <!-- ===== Preloader Start ===== -->
     <div x-show="loaded" x-init="window.addEventListener('DOMContentLoaded', () => { setTimeout(() => loaded = false, 500) })"
         class="fixed left-0 top-0 z-999999 flex h-screen w-screen items-center justify-center bg-white dark:bg-black">
-        <div class="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent">
+        <div class="h-16 w-16 animate-spin rounded-full border-4 border-solid border-[#9061f9] border-t-transparent">
         </div>
     </div>
     <!-- ===== Preloader End ===== -->
@@ -31,8 +31,7 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
     <div class="flex h-screen overflow-hidden">
         <x-partials.admin.sidenav />
         <!-- ===== Content Area Start ===== -->
-        <div
-            class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden bg-purple-50 dark:bg-black">
+        <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden bg-purple-50 dark:bg-black">
             <!-- ===== Header Start ===== -->
             <x-partials.admin.header-component />
             <!-- ===== Header End ===== -->
