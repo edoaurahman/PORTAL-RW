977f3a217fc4afaa36cc26afb23bbafc597afe76
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-06-09 11:35:43 +0700

    refactor: Update login page labels for better clarity

--- resources/views/components/view/login.blade.php
+++ resources/views/components/view/login.blade.php
@@ -26,15 +26,15 @@
                 <h3 class="text-2xl font-sans font-normal capitalize mb-8">Masuk ke akun anda</h3>
 
                 <div class="mb-5">
-                    <label for="username"
-                        class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">NIK</label>
+                    <label for="username" class="block mb-2 text-lgfont-medium text-gray-900 dark:text-white">
+                        Username</label>
                     <input type="text" id="username"
                         class="shadow-sm bg-white border border-gray-300 text-gray-900 text-sm  focus:ring-ungu focus:border-ungu block w-full py-3 px-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                         placeholder="NIK" required name="username" />
                 </div>
                 <div class="mb-5">
                     <label for="password" class="block mb-2 text-lgfont-medium text-gray-900 dark:text-white">
-                        password</label>
+                        Password</label>
                     <input type="password" id="password"
                         class="shadow-sm bg-white border border-gray-300 text-gray-900 text-sm  focus:ring-ungu focus:border-ungu block w-full py-3 px-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                         placeholder="Password" name="password" required />
