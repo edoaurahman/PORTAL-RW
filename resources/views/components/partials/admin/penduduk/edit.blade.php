04206de4b3acd08ccbbfadcbd64bf82c234060ad
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-06-11 09:27:56 +0700

    refactor: Remove required attribute from image input in penduduk edit form

--- resources/views/components/partials/admin/penduduk/edit.blade.php
+++ resources/views/components/partials/admin/penduduk/edit.blade.php
@@ -292,7 +292,7 @@
 
                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                             for="file_input">Upload foto</label>
-                        <input required name="image" accept="image/*"
+                        <input name="image" accept="image/*"
                             class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                             aria-describedby="file_input_help" id="gambar_profile" type="file">
                         <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG,
